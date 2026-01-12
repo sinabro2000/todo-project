7fc366dbfa96f6c7fcc45f2340fd8ec37de95e37
Sinabro <rldjr8117@gmail.com>
2026-01-12 23:32:54 +0900

    구글 로그인 기능 추가

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { AuthProvider } from "./context/AuthContext";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
 
 createRoot(document.getElementById('root')).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>
+  <GoogleOAuthProvider clientId="327038395876-5gd9lmrp2fa2sca2l90e6cl3a8ugv05u.apps.googleusercontent.com">
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </GoogleOAuthProvider>
 )
